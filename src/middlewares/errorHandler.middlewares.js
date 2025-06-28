6910986b4f13abb8ddcc07059802d69d259129b0
Joule-1 <pandeyakshay301@gmail.com>
2025-06-28 17:04:28 +0530

    Add user model with validation and password handling; update environment variables and package dependencies

--- src/middlewares/errorHandler.middlewares.js
+++ src/middlewares/errorHandler.middlewares.js
@@ -1,15 +1,15 @@
 import { ApiError } from "../utils/ApiError.js";
 
-const errorHandler = (error, request, resposne, next) => {
+const errorHandler = (error, request, response, next) => {
     if (error instanceof ApiError) {
-        return resposne.status(error.statusCode || 400).json({
+        return response.status(error.statusCode || 400).json({
             statusCode: error.statusCode,
             success: false,
             data: null,
             message: error.message,
         });
     } else {
-        return resposne.status(500).json({
+        return response.status(500).json({
             statusCode: 500,
             success: false,
             data: null,
---
+++ src/models/user.model.js
@@ -0,0 +1,100 @@
+import mongoose, { Schema } from "mongoose";
+import validator from "validator";
+import bcyrpt from "bcrypt";
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: [true, "Username is required"],
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true,
+            minlength: [3, "Username should not be less than 3 characters"],
+            maxlength: [100, "Username should not exceed 100 characters"],
+        },
+        email: {
+            type: String,
+            required: [true, "Email is required"],
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true,
+            minlength: [6, "Email should not be less than 6 characters"],
+            maxlength: [254, "Username should not exceed 254 characters"],
+            validate: [validator.isEmail, "Invalid email format"],
+            set: (value) => validator.normalizeEmail(value),
+        },
+        fullname: {
+            type: String,
+            required: [true, "Full name is required"],
+            trim: true,
+            minlength: [3, "Full name should not be less than 3 characters"],
+            maxlength: [100, "Full name should not exceed 100 characters"],
+        },
+        avatar: {
+            type: String,
+            required: [true, "Avatar is required"], // cloudinary URL
+        },
+        coverImage: {
+            type: String, // cloudinary URL
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video",
+            },
+        ],
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+            validate: [validator.isStrongPassword, "Invalid password"],
+        },
+        refreshToken: {
+            type: String,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcyrpt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcyrpt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname,
+        },
+        process.env.ACCESS_TOKEN_EXPIRY,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+        }
+    );
+};
+
+userSchema.methods.generateRefereshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+        }
+    );
+};
+
+export const User = mongoose.model("User", userSchema);
