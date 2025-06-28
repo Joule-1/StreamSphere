3e7900fc7a59161435cc5d94611f8b17acb050de
Joule-1 <pandeyakshay301@gmail.com>
2025-06-29 00:35:05 +0530

    Implement user registration logic with validation and file upload handling

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -26,6 +26,97 @@ const generateAccessAndRefreshTokens = async (userId) => {
     }
 };
 
-const registerUser = asyncHandler(async(req, res) => {
-    
-})
+const registerUser = asyncHandler(async (req, res) => {
+    const { fullname, email, username, password } = req.body;
+
+    if (
+        [fullname, email, username, password].some(
+            (field) => field?.trim() === ""
+        )
+    ) {
+        throw new ApiError(400, "All fields are required");
+    }
+
+    const existedUser = await User.findOne({
+        $or: [{ username }, { email }],
+    });
+
+    if (existedUser)
+        throw new ApiError(409, "User with email or username already exists");
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+
+    let coverImageLocalPath;
+    if (
+        req.files &&
+        Array.isArray(req.files.coverImage) &&
+        req.files.coverImage.length > 0
+    )
+        coverImageLocalPath = req.files.coverImage[0].path;
+
+    if (!avatarLocalPath) throw new ApiError(400, "Avatar file is required");
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    if (!avatar) throw new ApiError(500, "Error in uploading file");
+
+    if (!avatar?.secure_url)
+        throw new ApiError(500, "Failed to get avatar URL");
+
+    const user = await User.create({
+        username,
+        email,
+        fullname,
+        avatar: avatar?.secure_url,
+        coverImage: coverImage?.secure_url || "",
+        password,
+    });
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+        user._id
+    );
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    );
+
+    if (!createdUser)
+        throw new ApiError(
+            500,
+            "Something went wrong while registering the user"
+        );
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    };
+
+    return res
+        .status(201)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            new ApiResponse(201, createdUser, "User Registered Successfully")
+        );
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, username, password } = req.body;
+
+    if (!username && !email)
+        throw new ApiError(400, "Username or Email is required");
+
+    const user = await User.findOne({
+        $or: [{ username }, { email }],
+    });
+
+    if(!user)
+        throw new ApiError(400, "User doesn't exist")
+
+});
+
+return {
+    registerUser,
+     
+};
